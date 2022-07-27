5c442ae32985e4c1ed98a7a5408c988812e084d5
matheusgb <matheusgbr98@gmail.com>
2022-07-27 00:37:25 -0300

    feat: create user model

---
+++ src/database/models/user.js
@@ -0,0 +1,26 @@
+'use strict';
+//JSdocs
+  /**
+   * @param {import('sequelize').Sequelize } sequelize 
+   * @param {import('sequelize').DataTypes} DataTypes 
+   */
+const createUserModel = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    displayName: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    image: DataTypes.STRING,
+  }, {
+    tableName: 'Users',
+    timestamps: false,
+  })
+  return User;
+};
+
+module.exports = createUserModel
